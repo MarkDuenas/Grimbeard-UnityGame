40df5dd0dbf369c5aefae67a1fd4755994a21c22
Nolan Wallace <NolanrWallace03@gmail.com>
2020-06-22 17:19:39 -0700

    MM & Collisions
    basic collisions, movement no unlimeted jumping

---
+++ CatHero Project/Assets/PlayerScripts/player_move_test.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class player_move_test : MonoBehaviour
+{
+    public int playerSpeed = 6;
+    private bool facingRIght = true;
+    public int playerJumpPower = 500;
+    private float MoveX;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+
+    }
+
+    void PlayerMove()
+    {
+    
+        //CONTROLS
+        MoveX = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown ("Jump"))
+        {
+            Jump();
+        }
+        //ANIAMTION
+        //PLAYER DIRECTION
+        if(MoveX > 0.0f && facingRIght == false)
+        {
+            FlipPlayer();
+        }
+        else if (MoveX < 0.0f && facingRIght == true)
+        {
+            FlipPlayer();
+        }
+        //PHYSICS
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (MoveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        if(gameObject.GetComponent<Rigidbody2D>().velocity.y < .5 && gameObject.GetComponent<Rigidbody2D>().velocity.y > -.5)
+        {
+            GetComponent<Rigidbody2D>().AddForce (Vector2.up * playerJumpPower);
+        }
+        //Jumping Code
+    }
+
+    void FlipPlayer()
+    {
+        facingRIght = !facingRIght;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+}
