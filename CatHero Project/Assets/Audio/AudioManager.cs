baa62361edd0bcba9d327e6cb98a59c8e474397e
Nolan Wallace <NolanrWallace03@gmail.com>
2020-06-23 21:17:11 -0700

    intro music to level music transistion
    intro music background music bigger menu text

--- CatHero Project/Assets/Audio/AudioManager.cs
+++ CatHero Project/Assets/Audio/AudioManager.cs
@@ -6,9 +6,20 @@ public class AudioManager : MonoBehaviour
 {
 
     public Sound[] sounds;
+    public static AudioManager instantce;
     // Start is called before the first frame update
     void Awake()
     {
+        if(instantce == null)
+        {
+            instantce = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
         foreach(Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -24,9 +35,14 @@ public class AudioManager : MonoBehaviour
         s.source.Play();
     }
 
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
+    }
     private void Start() 
         {
-            Play("BackgroundMusic");
+            Play("IntroMusic");
         }
     // Update is called once per frame
 }
