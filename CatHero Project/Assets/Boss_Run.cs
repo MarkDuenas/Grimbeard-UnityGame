1aad61e09d55d74088932beb5b9e50d5e3763ca4
Nolan Wallace <NolanrWallace03@gmail.com>
2020-06-24 20:33:18 -0700

    boss scripts

---
+++ CatHero Project/Assets/Boss_Run.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Run : StateMachineBehaviour
+{
+    public float speed = 10f;
+
+    public float attackRange = 2f;
+
+    public int nextAttacktime = 200;
+    public int attackTimer = 0;
+
+    public int distance = 20;
+
+    Transform player;
+    Rigidbody2D rb;
+    Boss boss;
+
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<Boss>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.LookAtPlayer();
+
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        if (player.position.x >= 255f)
+        {
+            Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+            rb.MovePosition(newPos);
+        }
+
+
+        if(Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            attackTimer += 1;
+            if(attackTimer == nextAttacktime)
+            {
+                animator.SetTrigger("StabAttack");
+                attackTimer = 0;
+            }
+
+            }
+        }
+
+            
+                
+                
+                    
+
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("StabAttack");
+    }
+}
+
