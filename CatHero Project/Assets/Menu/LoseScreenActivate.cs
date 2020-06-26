84f9842de68566e9ffbbed69e487bfae47126c38
Nolan Wallace <NolanrWallace03@gmail.com>
2020-06-26 12:47:19 -0700

    menu change

--- CatHero Project/Assets/Menu/LoseScreenActivate.cs
+++ CatHero Project/Assets/Menu/LoseScreenActivate.cs
@@ -19,7 +19,7 @@ public class LoseScreenActivate : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        FindObjectOfType<GameManager>().Win();
+        FindObjectOfType<GameManager>().Loss();
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
